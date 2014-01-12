ca3c14150535a5a091c38af64e1364e0e575e71d
Javad Karabi <karabijavad@gmail.com>
2014-01-11 20:43:19 -0500

    more node stuff

--- lib/cadet/node.rb
+++ lib/cadet/node.rb
@@ -3,6 +3,18 @@ module Cadet
     def self.create(db)
       db.createNode()
     end
+    def self.addLabel(node, label)
+      node.addLabel(org.neo4j.graphdb.DynamicLabel.label(label))
+    end
+    def self.getNodeById(db, id)
+      db.getNodeById(id)
+    end
+    def self.getRelationshipById(db, id)
+      db.getRelationshipById(id)
+    end
+    def self.findNodesByLabelAndProperty(db, label, key, value)
+      db.findNodesByLabelAndProperty(label, key, value)
+    end
   end
 end
 
