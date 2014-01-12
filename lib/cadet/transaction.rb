fd4f64ffe2d1c190f71e2ec3a2b8a186b12eb7eb
Javad Karabi <karabijavad@gmail.com>
2014-01-11 20:19:38 -0500

    pass block to transaction

--- lib/cadet/transaction.rb
+++ lib/cadet/transaction.rb
@@ -1,6 +1,17 @@
 module Cadet
   module Transaction
 
+    def self.transaction(db)
+      tx = db.beginTx()
+
+      begin
+        yield tx
+        tx.success()
+      ensure
+        tx.close()
+      end
+    end
+
     def self.begin(db)
       db.beginTx()
     end
