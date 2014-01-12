dfa90daa24d358e0a06fde9fe03b337a386fe12f
Javad Karabi <karabijavad@gmail.com>
2014-01-11 21:20:30 -0500

    better

--- lib/cadet/session.rb
+++ lib/cadet/session.rb
@@ -1,10 +1,21 @@
 module Cadet
-  module Session
-    def self.open(location)
-      org.neo4j.graphdb.factory.GraphDatabaseFactory.new.newEmbeddedDatabase(location)
+  class Session
+    def initialize(db)
+      @db = db
     end
-    def self.close(db)
-      db.shutdown()
+    def self.open(location)
+      new(org.neo4j.graphdb.factory.GraphDatabaseFactory.new.newEmbeddedDatabase(location))
+    end
+    def close
+      @db.shutdown()
+    end
+    def transaction
+      Cadet::Transaction.transaction(@db) do
+        yield
+      end
+    end
+    def create_node
+      Cadet::Node.create(@db)
     end
   end
 end
