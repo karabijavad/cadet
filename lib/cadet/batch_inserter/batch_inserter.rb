3538add37bdb33d1ddcbf1e244fb9dc629c54641
Javad Karabi <karabijavad@gmail.com>
2014-01-26 22:49:43 -0500

    constraints for batchinsertion

--- lib/cadet/batch_inserter/batch_inserter.rb
+++ lib/cadet/batch_inserter/batch_inserter.rb
@@ -3,8 +3,15 @@ module Cadet
     include_package "org.neo4j.unsafe.batchinsert"
 
     def self.open(location)
-      new BatchInserters.inserter(location)
+      @db = new BatchInserters.inserter(location)
     end
 
+    def transaction
+      yield
+    end
+
+    def constraint(label, property)
+      @db.createDeferredConstraint(org.neo4j.graphdb.DynamicLabel.label(label)).assertPropertyIsUnique(property)
+    end
   end
 end
