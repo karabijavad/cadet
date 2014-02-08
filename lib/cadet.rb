5514def1c55019dba508ff05c0deee0cbfe151d2
Javad Karabi <karabijavad@gmail.com>
2014-02-07 23:39:17 -0600

    neo4j 2.0.1

--- lib/cadet.rb
+++ lib/cadet.rb
@@ -3,11 +3,11 @@ require 'java'
 [
 "concurrentlinkedhashmap-lru-1.3.1.jar",
 "lucene-core-3.6.2.jar",
-"neo4j-graph-algo-2.0.0.jar",
-"neo4j-graph-matching-2.0.0.jar",
-"neo4j-kernel-2.0.0.jar",
-"neo4j-lucene-index-2.0.0.jar",
-"neo4j-shell-2.0.0.jar"
+"neo4j-graph-algo-2.0.1.jar",
+"neo4j-graph-matching-2.0.1.jar",
+"neo4j-kernel-2.0.1.jar",
+"neo4j-lucene-index-2.0.1.jar",
+"neo4j-shell-2.0.1.jar"
 ].each { |file| require "neo4j/#{file}" }
 
 require 'cadet/session'
