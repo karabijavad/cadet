d7051a6e7ab7ced8f49d280db491cf1ea08685c3
Javad Karabi <karabijavad@gmail.com>
2014-02-21 02:09:07 -0600

    cleanup

--- spec/unit/batch_insert_spec.rb
+++ spec/unit/batch_insert_spec.rb
@@ -7,18 +7,18 @@ describe Cadet::BatchInserter do
     Dir.mktmpdir do |tmpdir|
       db = Cadet::BatchInserter::Session.open(tmpdir)
       db.transaction do
-        javad     = db.get_node :Person,  :name, "Javad"
-        ellen     = db.get_node :Person,  :name, "Ellen"
+        javad     = db.get_a_Person_by_name "Javad"
+        ellen     = db.get_a_Person_by_name "Ellen"
 
-        javad.outgoing(:lives_in) << db.get_node(:City, :name, "Chicago")
-        ellen.outgoing(:lives_in) << db.get_node(:City, :name, "Chicago")
+        javad.outgoing(:lives_in) << db.get_a_City_by_name("Chicago")
+        ellen.outgoing(:lives_in) << db.get_a_City_by_name("Chicago")
       end
       db.close
 
       db = Cadet::Session.open(tmpdir)
       db.transaction do
-        javad     = db.get_node :Person,  :name, "Javad"
-        ellen     = db.get_node :Person,  :name, "Ellen"
+        javad     = db.get_a_Person_by_name "Javad"
+        ellen     = db.get_a_Person_by_name "Ellen"
 
         javad.outgoing(:lives_in).to_a.should == ellen.outgoing(:lives_in).to_a
       end
