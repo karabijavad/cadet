d2c6d06c13dc1c71b66b5bfe3007e4b2afd2f7f8
Javad Karabi <karabijavad@gmail.com>
2014-02-22 15:00:56 -0600

    dont need this

--- lib/cadet/dynamic_label.rb
+++ lib/cadet/dynamic_label.rb
@@ -3,7 +3,7 @@ module Cadet
     @dynamic_labels = {}
 
     def self.label(name)
-      @dynamic_labels[name] ||= org.neo4j.graphdb.DynamicLabel.label(name.to_s)
+      @dynamic_labels[name] ||= org.neo4j.graphdb.DynamicLabel.label(name)
     end
   end
 end
--- lib/cadet/dynamic_relationshiptype.rb
+++ lib/cadet/dynamic_relationshiptype.rb
@@ -3,7 +3,7 @@ module Cadet
     @dynamic_relationship_types = {}
 
     def self.withName(name)
-      @dynamic_relationship_types[name] ||= org.neo4j.graphdb.DynamicRelationshipType.withName(name.to_s)
+      @dynamic_relationship_types[name] ||= org.neo4j.graphdb.DynamicRelationshipType.withName(name)
     end
   end
 end
