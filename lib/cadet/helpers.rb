5139114b0ec912bcb1e83583feede3c8f328c0ae
Javad Karabi <karabijavad@gmail.com>
2014-02-28 13:24:02 -0600

    explanation

--- lib/cadet/helpers.rb
+++ lib/cadet/helpers.rb
@@ -1,3 +1,12 @@
+# This is to fix a (bug?) in jruby.
+# typically, when a ruby symbol is passed into a native java function,
+# it should be converted to a java string by jruby.
+# unfortunately, this conversion does not seem to take place when the native
+# java function is overloaded.
+# This method allows me to specify :symbol.to_java_string,
+# so that I do not need to create a Ruby string from a symbol,
+# just so that i can get a native java string
+
 class Symbol
   JString = java.lang.String
   def to_java_string
