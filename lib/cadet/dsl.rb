2b4ed7aa2577bf8bba2fe9893015e6b2bba75850
Javad Karabi <karabijavad@gmail.com>
2014-03-01 12:33:32 -0600

    oops

--- lib/cadet/dsl.rb
+++ lib/cadet/dsl.rb
@@ -6,27 +6,19 @@ module Cadet
 
     def method_missing(name, *args, &block)
       case name
-        when /^([A-z_]*)_by_([A-z_]*)/
+        when /^([A-z_]*)_by_([A-z_]*)$/
+            self.class.class_eval "
+              def #{name}(value)
+                @db.get_node :#{$1}, :#{$2}, value
+              end"
+            return self.send(name, *args, &block)
+
+        when /^create_([A-z_]*)$/
           self.class.class_eval "
-            def #{name}(value)
-              @db.get_node :#{$1}, :#{$2}, value
+            def #{name}(value, indexing_property = nil)
+              @db.create_node :#{$1}, value, indexing_property
             end"
           return self.send(name, *args, &block)
-
-        when /^create_([A-z_]*)_on_([A-z_]*)/.match(name)
-          self.class.class_eval "
-            def #{name}(value)
-              @db.create_node :#{$1}, value, :#{$2}
-            end"
-          return self.send(name, *args, &block)
-
-        when /^create_([A-z_]*)/
-          self.class.class_eval "
-            def #{name}(value = {})
-              @db.create_node :#{$1}, value
-            end"
-          return self.send(name, *args, &block)
-
         else
           return @db.send(name, *args, &block)
       end
