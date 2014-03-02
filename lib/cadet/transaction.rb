1728aed9acb0742a9402a4c4502c7ccba8b23176
Javad Karabi <karabijavad@gmail.com>
2014-03-01 20:31:29 -0600

    dsl is not via the transaction now

--- lib/cadet/transaction.rb
+++ lib/cadet/transaction.rb
@@ -7,21 +7,11 @@ module Cadet
       @underlying = @session.begin_tx
     end
 
-    def method_missing(name, *args, &block)
-      # for the "dsl".
-      # the transaction block is instance_eval'd by this class,
-      # so any missing methods are then sent to the session
-      # essentially means that session.blah can then be writen blah
-      @session.send(name, *args, &block)
-
-    end
-
     def success
       @underlying.success
     end
     def close
       @underlying.close
     end
-
   end
 end
