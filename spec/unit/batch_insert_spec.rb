cda7bc0b07017d5b4aca3e2592a687e7f2f8726e
Javad Karabi <karabijavad@gmail.com>
2014-03-04 14:36:46 -0600

    cleaner

--- spec/unit/batch_insert_spec.rb
+++ spec/unit/batch_insert_spec.rb
@@ -26,13 +26,9 @@ describe Cadet::BatchInserter do
       self.class.should == Cadet::Session
 
       transaction do
-        javad   = Person_by_name("Javad")
-        houston = City_by_name("Houston")
-        chicago = City_by_name("Chicago")
-
         # the order matters here, unfortunately.
         # TODO: find a way for [chicago, houston] to also work
-        javad.outgoing(:lives_in).should == [houston, chicago]
+        Person_by_name("Javad").outgoing(:lives_in).should == [City_by_name("Houston"), City_by_name("Chicago")]
       end
     end
   end
